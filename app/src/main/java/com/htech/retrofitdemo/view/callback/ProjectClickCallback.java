2be30b19e6bed55dae7786a31e06946b776d5f2c
Hemant Patel <hpatel@officebrain.com>
2018-03-06 17:28:19 +0530

    android mvvm example with livedata and databindig

---
+++ app/src/main/java/com/htech/retrofitdemo/view/callback/ProjectClickCallback.java
@@ -0,0 +1,7 @@
+package com.htech.retrofitdemo.view.callback;
+
+import com.htech.retrofitdemo.service.model.Project;
+
+public interface ProjectClickCallback {
+    void onClick(Project project);
+}
